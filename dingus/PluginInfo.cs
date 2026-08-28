557d033214fe16d8c00a373908455908a005d04a
rickystuff <doxxer.frame@gmail.com>
2026-08-28 22:51:26 +1200

    deleted dir to upload the files later

--- dingus/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace dingus
-{
-    internal class PluginInfo
-    {
-        public const string GUID = "luna.dingus";
-        public const string Name = "dingus";
-        public const string Version = "1.0.0";
-    }
-}
